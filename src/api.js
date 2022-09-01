25e543dff2aaeb9f82e9031c47ee9e0933556053
Nakano <hirota7771@rakuten.jp>
2022-09-01 11:27:20 +0900

    revise api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL : "https://commoard-api.herokuapp.com" ,
+    baseURL : process.env.REACT_APP_API_BASE_URL ,
     withCredentials : true
 });
