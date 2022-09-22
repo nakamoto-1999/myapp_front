5bc84a84727deba0afd12df8bd38bc291f85cbbd
Nakano <hirota7771@rakuten.jp>
2022-09-22 19:01:48 +0900

    revise index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <CookiesProvider>
         <App />
-    </CookiesProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
