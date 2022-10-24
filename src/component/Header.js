889ccdc8a3251cf97bb503beef3b950ca0156fc5
Nakano <hirota7771@rakuten.jp>
2022-10-24 14:52:33 +0900

    revise header

--- src/component/Header.js
+++ src/component/Header.js
@@ -66,7 +66,7 @@ const Header = ()=>{
 
         </div>
 
-        <div style={{marginTop : "5px"}}>
+        <div style={{marginTop : "5px" , marginBottom : "5px"}}>
             <ThreadSearchForm/>
         </div>
         
